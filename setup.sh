c45199609ee027b1f158db7e48478d447fb9f7e1
rinx <rintaro.okamura@gmail.com>
2013-04-07 22:59:21 +0900

    gitconfig,gitignore added

--- setup.sh
+++ setup.sh
@@ -6,5 +6,7 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
 
 
