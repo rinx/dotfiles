b29ea96fa2507539b99d8faa83ec7f411a0451bd
Rintaro Okamura <rintaro.okamura@gmail.com>
2013-09-07 21:11:06 +0900

    auto-completion more useful

--- zsh.d/basic.zsh
+++ zsh.d/basic.zsh
@@ -6,6 +6,26 @@ compinit
 #highlight for completion
 zstyle ':completion:*:default' menu select=2 
 
+#completion settings
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
+zstyle ':completion:*:warnings' format '%F{RED}No matches for:''%F{YELLOW} %d'$DEFAULT
+zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+#separators for completion
+zstyle ':completion:*' list-separator '-->'
+zstyle ':completion:*:manuals' separate-sections true
+
+autoload colors
+colors
+
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
 #vimlike keybind
 bindkey -v 
 
@@ -32,16 +52,12 @@ SAVEHIST=50000
 setopt hist_ignore_dups
 setopt share_history
 
-#auto_cd
 setopt auto_cd
 setopt auto_pushd
 
 setopt correct
 setopt list_packed
 setopt nolistbeep
-
-#autoload predict-on
-#predict-on
 
 setopt prompt_subst
 setopt transient_rprompt
