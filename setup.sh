8cc8f95b72dd47675ae9e71fed9c4226614130e9
Rintaro Okamura <rintaro.okamura@gmail.com>
2014-01-30 21:56:25 +0900

    Add failing message in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,41 +4,70 @@
 
 if [ ! -f $HOME/.vimrc ]; then
     ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-    echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.vimrc created\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.vimrc created\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA symbolic link $HOME/.vimrc creating failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 if [ ! -f $HOME/.zshrc ]; then
     ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-    echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.zshrc created\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.zshrc created\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA symbolic link $HOME/.zshrc creating failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 if [ ! -f $HOME/.tmux.conf ]; then
     ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-    echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.tmux.conf created\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.tmux.conf created\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA symbolic link $HOME/.tmux.conf creating failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 if [ ! -f $HOME/.gitconfig ]; then
     ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-    echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.gitconfig created\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.gitconfig created\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA symbolic link $HOME/.gitconfig creating failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 if [ ! -f $HOME/.gitignore ]; then
     ln -s $HOME/.dotfiles/.gitignore $HOME/.gitignore
-    echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.gitignore created\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.gitignore created\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA symbolic link $HOME/.gitignore creating failed\033[00m" | sed "s/^-e //"
+    fi
+
 fi
 
 #for tmux statusline memory-status
 if [ ! -x $HOME/.bin/used-mem ]; then
     wget https://raw.github.com/yonchu/used-mem/master/used-mem -P $HOME/.bin > /dev/null 2>&1
     chmod +x $HOME/.bin/used-mem
-    echo -e "\033[0;32m✔ \033[1;36mA executable file 'used-mem' downloaded in $HOME/.bin\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;36mA executable file 'used-mem' downloaded in $HOME/.bin\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA executable file 'used-mem' downloading failed\033[00m" | sed "s/^-e //"       
+    fi
 fi
 
 #for tmux statusline battery-status
 if [ ! -x $HOME/.bin/battery ]; then
     wget https://raw.github.com/richo/battery/master/bin/battery -P $HOME/.bin > /dev/null 2>&1
     chmod +x $HOME/.bin/battery
-    echo -e "\033[0;32m✔ \033[1;36mA executable file 'battery' downloaded in $HOME/.bin\033[00m" | sed "s/^-e //"
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;36mA executable file 'battery' downloaded in $HOME/.bin\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mA executable file 'battery' downloading failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 
@@ -46,6 +75,11 @@ fi
 if [ ! -d $HOME/.vim/bundle/neobundle.vim ]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m✔ \033[1;36mneobundle.vim cloned in $HOME/.vim/bundle/neobundle.vim\033[00m" | sed "s/^-e //"
+    else
+        echo -e "\033[0;31m✗ \033[1;31mneobundle.vim cloning failed\033[00m" | sed "s/^-e //"
+    fi
 fi
 
 # vim filetype plugins
