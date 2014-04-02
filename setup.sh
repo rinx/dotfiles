4689f75d3a2cd28767699f985eb1b0dbc3255633
Rintaro Okamura <rintaro.okamura@gmail.com>
2014-04-02 11:50:51 +0900

    Revise setup.sh script to get absolute dotfiles path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CMDNAME=`basename $0`
+DOTDIR=$(cd $(dirname $0); pwd)
 
 while getopts acfstv OPT
 do
@@ -22,7 +23,7 @@ if [ "$FLG_A" = "TRUE" ] || [ "$FLG_S" = "TRUE" ]; then
     for filename in vimrc zshrc tmux.conf gitconfig gitignore ; do
         if [ ! -f $HOME/.$filename ] || [ "$FLG_F" = "TRUE" ]; then
             [ "$FLG_F" = "TRUE" ] && [ -f $HOME/.$filename ] && rm -f $HOME/.$filename
-            ln -s $HOME/.dotfiles/$filename $HOME/.$filename
+            ln -s $DOTDIR/$filename $HOME/.$filename
             if [ $? -eq 0 ]; then
                 echo -e "\033[0;32m✔ \033[1;35mA symbolic link $HOME/.$filename created\033[00m" | sed "s/^-e //"
             else
