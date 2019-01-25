22dd4c23ab3ca719b9562530dafcb516c73c2ecb
Rintaro Okamura <rintaro.okamura@gmail.com>
2019-01-25 21:54:12 +0900

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,7 @@
 {:user
  {:plugins
-  [[refactor-nrepl "2.4.0-SNAPSHOT"]
-   [cider/cider-nrepl "0.18.0"]
-   [lein-kibit "0.1.5"]
-   [jonase/eastwood "0.2.5"]]
-  :dependencies
-  [[org.clojure/tools.nrepl "0.2.12"]
-   [cljfmt "0.6.0"]]
+  [[refactor-nrepl "2.4.0"]
+   [cider/cider-nrepl "0.20.0"]]
   :repl-options
   {:nrepl-middleware [cider.nrepl/wrap-complete
                       cider.nrepl/wrap-format
