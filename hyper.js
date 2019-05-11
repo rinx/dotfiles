06ce02e687527feafbc9175121380895ad71961b
Rintaro Okamura <rintaro.okamura@gmail.com>
2019-05-11 14:53:08 +0900

    Revise fontsize of hyper.js

--- hyper.js
+++ hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 10,
 
     // font family with optional fallbacks
     fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
