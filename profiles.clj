1273bf6423c50df65f0af5e4e76e19fc75550c22
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-02-02 16:31:03 +0900

    [patch] :heavy_plus_sign: Add repl option
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,7 @@
 {:user
  {:plugins [[refactor-nrepl "2.4.0"]
             [cider/cider-nrepl "0.22.4"]]
-  :dependencies [[nrepl "0.6.0"]]}}
+  :dependencies [[nrepl "0.6.0"]]}
+ :repl
+ {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+  :repl-options {:timeout 300000}}}
