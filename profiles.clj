3f62cbf9d2570a2d4f4685950d41eb9ad7f39f9a
Rintaro Okamura <rintaro.okamura@gmail.com>
2021-04-29 18:44:28 +0900

    :arrow_up: upgrade libs
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 {:user
- {:plugins [[refactor-nrepl "2.4.0"]
-            [cider/cider-nrepl "0.22.4"]]
-  :dependencies [[nrepl "0.6.0"]]}
+ {:plugins [[refactor-nrepl "2.5.0"]
+            [cider/cider-nrepl "0.26.0"]]
+  :dependencies [[nrepl "0.8.3"]]}
  :repl
- {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
   :repl-options {:timeout 300000}}}
