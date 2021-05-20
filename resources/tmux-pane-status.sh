d600d2bc4497c88f4a42cd4f904820c87243ea0f
Rintaro Okamura <rintaro.okamura@gmail.com>
2021-05-20 16:10:37 +0900

    :wrench: Revise colors
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- resources/tmux-pane-status.sh
+++ resources/tmux-pane-status.sh
@@ -1,26 +1,14 @@
 #!/bin/zsh
 
-# if git_status=$(cd $1 && git status 2>/dev/null ); then
-#     git_branch="$(echo $git_status| awk 'NR==1 {print $3}')"
-#     case $git_status in
-#         *Changes\ not\ staged* ) state="#[bg=black,fg=magenta]  #[fg=default]" ;;
-#         *Changes\ to\ be\ committed* ) state="#[bg=black,fg=blue]  #[default]" ;;
-#         * ) state="#[bg=black,fg=green]  #[default]" ;;
-#     esac
-#     git_info="#[underscore]#[bg=black,fg=blue][ ${git_branch}]#[default]${state}"
-# else
-#     git_info=""
-# fi
-
 if k8s_context="$(kubectl config current-context 2>/dev/null)"; then
     # k8s_user="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$k8s_context\")].context.user}")"
     k8s_ns="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$k8s_context\")].context.namespace}")"
     [[ -z "$k8s_ns" ]] && k8s_ns="default"
-    k8s_info="#[underscore]#[bg=black,fg=yellow][ ${k8s_context}/${k8s_ns}]#[default]"
+    k8s_info="#[underscore]#[bg=default,fg=yellow][ﴱ ${k8s_context}/${k8s_ns}]#[default]"
 else
     k8s_info=""
 fi
 
-directory="#[underscore]#[bg=black,fg=cyan][ $1]#[default]"
+directory="#[underscore]#[bg=default,fg=cyan][ $1]#[default]"
 
 echo "$directory $k8s_info"
