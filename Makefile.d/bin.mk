70cbcccae729141f13125a56adc06cbe063c3762
Rintaro Okamura <rintaro.okamura@gmail.com>
2022-09-24 12:03:35 +0000

    [patch] :robot: Automatically upgrade dependencies.
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- Makefile.d/bin.mk
+++ Makefile.d/bin.mk
@@ -19,7 +19,7 @@
 ## ---   :tx "(fn [x] (-> x (string/replace #\"ci_\" \"v\") (string/replace #\"_.*\" \"\")))"}]
 
 BAT_VERSION := v0.22.1
-BB_VERSION := 0.9.162
+BB_VERSION := 0.10.163
 DELTA_VERSION := 0.14.0
 EXA_VERSION := v0.10.1
 FD_VERSION := v8.4.0
