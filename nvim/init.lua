9f8ed77e871ad367a32b0fd9e73f8ae425b076da
Rintaro Okamura <rintaro.okamura@gmail.com>
2022-12-02 19:18:04 +0900

    update init.lua
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,14 @@
+local execute = vim.api.nvim_command
 local fn = vim.fn
 
 local pack_path = fn.stdpath("data") .. "/site/pack"
 local fmt = string.format
 
 function ensure (user, repo)
-  local install_path = fmt("%s/packer/start/%s", pack_path, repo, repo)
+  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
   if fn.empty(fn.glob(install_path)) > 0 then
-    vim.cmd(fmt("silent !git clone https://github.com/%s/%s %s", user, repo, install_path))
-    vim.cmd(fmt("packadd %s", repo))
+    execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+    execute(fmt("packadd %s", repo))
   end
 end
 
