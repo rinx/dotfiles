2427abca994a28f98380bbd0f0ab701b9d667cb6
github-action-benchmark <github@users.noreply.github.com>
2023-01-27 13:26:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e7d60b354f8314dd1e1aed2911047d1bcd9ec1f0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674825793346,
+  "lastUpdate": 1674825962796,
   "repoUrl": "https://github.com/rinx/dotfiles",
   "entries": {
     "Benchmark": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim load time",
             "value": 3708.4389140999992,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "committer": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "distinct": true,
+          "id": "e7d60b354f8314dd1e1aed2911047d1bcd9ec1f0",
+          "message": "fix: fix benchmark workflow\n\nfix nvim plugin install script\n\nSigned-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>",
+          "timestamp": "2023-01-27T22:23:01+09:00",
+          "tree_id": "ff79f082ca567f185798a87f9f65136c64a96d14",
+          "url": "https://github.com/rinx/dotfiles/commit/e7d60b354f8314dd1e1aed2911047d1bcd9ec1f0"
+        },
+        "date": 1674825961364,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 61.280369900000004,
+            "unit": "ms"
+          },
+          {
+            "name": "neovim load time",
+            "value": 2996.7896395,
             "unit": "ms"
           }
         ]
