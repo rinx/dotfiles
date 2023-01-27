dae094959ba171abc2fe581d7891a662fc4ba032
github-action-benchmark <github@users.noreply.github.com>
2023-01-27 13:23:13 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9b74b45bf065800ecad0209f3bee976bed864dc9

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1674825793346,
+  "repoUrl": "https://github.com/rinx/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "committer": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "distinct": true,
+          "id": "9b74b45bf065800ecad0209f3bee976bed864dc9",
+          "message": "fix: fix benchmark workflow\n\nSigned-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>",
+          "timestamp": "2023-01-27T22:19:32+09:00",
+          "tree_id": "42238d8e08f2887044e8488786470dc3b16f4068",
+          "url": "https://github.com/rinx/dotfiles/commit/9b74b45bf065800ecad0209f3bee976bed864dc9"
+        },
+        "date": 1674825792388,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 78.72708330000002,
+            "unit": "ms"
+          },
+          {
+            "name": "neovim load time",
+            "value": 3708.4389140999992,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
