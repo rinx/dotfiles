92f50b72d0dc3db9ffbf43c7b4682429f37eb6ff
github-action-benchmark <github@users.noreply.github.com>
2023-01-27 14:55:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7cce51b41227cec2075ba9186be21f2d42776a6d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674827572904,
+  "lastUpdate": 1674831342861,
   "repoUrl": "https://github.com/rinx/dotfiles",
   "entries": {
     "Benchmark": [
@@ -169,6 +169,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim load time",
             "value": 4704.6621448000005,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "committer": {
+            "email": "rintaro.okamura@gmail.com",
+            "name": "Rintaro Okamura",
+            "username": "rinx"
+          },
+          "distinct": true,
+          "id": "7cce51b41227cec2075ba9186be21f2d42776a6d",
+          "message": "feat: add TSInstallEnsure command\n\nSigned-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>",
+          "timestamp": "2023-01-27T23:49:54+09:00",
+          "tree_id": "776eac0feaaae7f57e5cfa4f1501faa4e7a8a5c8",
+          "url": "https://github.com/rinx/dotfiles/commit/7cce51b41227cec2075ba9186be21f2d42776a6d"
+        },
+        "date": 1674831341987,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 57.5135127,
+            "unit": "ms"
+          },
+          {
+            "name": "neovim load time",
+            "value": 310.8248932,
             "unit": "ms"
           }
         ]
