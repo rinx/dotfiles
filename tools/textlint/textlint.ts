31fff157a04a30faa2eaaad059ca994c6d0d8710
Rintaro Okamura <rintaro.okamura@gmail.com>
2023-01-27 16:53:14 +0900

    config: add textlint-filter-comment
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- tools/textlint/textlint.ts
+++ tools/textlint/textlint.ts
@@ -2,6 +2,7 @@ import { TextLintEngine } from "npm:textlint";
 import { createFormatter } from "npm:@textlint/linter-formatter";
 import _ from "npm:textlint-rule-preset-ja-technical-writing";
 import _ from "npm:@proofdict/textlint-rule-proofdict";
+import _ from "npm:textlint-filter-rule-comments";
 const engine = new TextLintEngine();
 const results = await engine.executeOnFiles(Deno.args);
 const formatter = createFormatter({
