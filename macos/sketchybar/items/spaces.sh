a843f6ae56c3082dbe442f16f78335543bbd1b92
Rintaro Okamura <rintaro.okamura@gmail.com>
2023-04-14 12:07:44 +0900

    config: revise bar appearance
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- macos/sketchybar/items/spaces.sh
+++ macos/sketchybar/items/spaces.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 ############## PRIMARY DISPLAY SPACES ############## 
-SPACE_ICONS=("I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX" "X")
+SPACE_ICONS=(" " " " " " " " " " " " " " " " " " " ")
+# SPACE_ICONS=(" " " " " " " " " " " " " " " " " " " ")
+# SPACE_ICONS=(" " " " " " " " " " " " " " " " " " " ")
+
 SPACE_CLICK_SCRIPT="yabai -m space --focus \$SID 2>/dev/null"
 
 for i in "${!SPACE_ICONS[@]}"
@@ -14,6 +17,7 @@ do
         icon.color=$GREY \
         icon.padding_right=10 \
         icon.highlight_color=$CREAM \
+        icon.font="$FONT:Regular:16.0" \
         label.drawing=off \
         click_script="$SPACE_CLICK_SCRIPT"
 done
