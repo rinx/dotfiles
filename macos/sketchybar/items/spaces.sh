d13c7792c7db702d9aed46fd15b4bce1439f457c
Rintaro Okamura <rintaro.okamura@gmail.com>
2023-04-17 19:40:15 +0900

    config(sketchybar): add window icons
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- macos/sketchybar/items/spaces.sh
+++ macos/sketchybar/items/spaces.sh
@@ -5,8 +5,6 @@ SPACE_ICONS=(" " " " " " " " " " " " " " " " " " "
 # SPACE_ICONS=(" " " " " " " " " " " " " " " " " " " ")
 # SPACE_ICONS=(" " " " " " " " " " " " " " " " " " " ")
 
-SPACE_CLICK_SCRIPT="yabai -m space --focus \$SID 2>/dev/null"
-
 for i in "${!SPACE_ICONS[@]}"
 do
     sid=$(($i+1))
@@ -15,9 +13,12 @@ do
         --set space.$sid associated_space=$sid \
         icon=${SPACE_ICONS[i]} \
         icon.color=$GREY \
-        icon.padding_right=10 \
+        icon.padding_right=4 \
         icon.highlight_color=$CREAM \
+        label.font="$FONT:Regular:16.0" \
         icon.font="$FONT:Regular:16.0" \
-        label.drawing=off \
-        click_script="$SPACE_CLICK_SCRIPT"
+        background.height=18 \
+        background.corner_radius=4 \
+        script="$PLUGIN_DIR/spaces.sh" \
+        --subscribe space.$sid front_app_switched
 done
