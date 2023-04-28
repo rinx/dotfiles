df13268e416724b0fbb5733dca8fca50756cddff
Rintaro Okamura <rintaro.okamura@gmail.com>
2023-04-28 22:54:15 +0900

    config(sketchybar): use bb for network providers
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

---
+++ macos/sketchybar/rc/src/items/network.clj
@@ -0,0 +1,24 @@
+(ns items.network
+  (:require
+   [sketchybar]
+   [common]
+   [colors]
+   [fonts]))
+
+(defn -main [& args]
+  (sketchybar/exec
+   (sketchybar/add-item :network :right)
+   (sketchybar/set
+    :network
+    {:script (common/plugin-script "network.jar")
+     :update_freq 2
+     :background.color (colors/get :cream)
+     :background.corner_radius 4
+     :background.height 15
+     :icon.color (colors/get :black)
+     :icon.font (fonts/get :Medium 12.0)
+     :label.color (colors/get :black)
+     :label.font (fonts/get :Medium 12.0)})))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
---
+++ macos/sketchybar/rc/src/items/wifi.clj
@@ -0,0 +1,24 @@
+(ns items.wifi
+  (:require
+   [sketchybar]
+   [common]
+   [colors]
+   [fonts]))
+
+(defn -main [& args]
+  (sketchybar/exec
+   (sketchybar/add-item :wifi :right)
+   (sketchybar/set
+    :wifi
+    {:script (common/plugin-script "wifi.jar")
+     :update_freq 5
+     :background.color (colors/get :cream)
+     :background.corner_radius 4
+     :background.height 15
+     :icon.color (colors/get :black)
+     :icon.font (fonts/get :Medium 12.0)
+     :label.color (colors/get :black)
+     :label.font (fonts/get :Medium 12.0)})))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
