af5053a814a12e32efd1076bde2a589aa04dac56
Rintaro Okamura <rintaro.okamura@gmail.com>
2023-04-28 21:34:52 +0900

    config(sketchybarrc): use bb for mic and volume providers
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

---
+++ macos/sketchybar/rc/src/items/mic.clj
@@ -0,0 +1,25 @@
+(ns items.mic
+  (:require
+   [sketchybar]
+   [common]
+   [colors]
+   [fonts]))
+
+(defn -main [& args]
+  (sketchybar/exec
+   (sketchybar/add-item :mic :right)
+   (sketchybar/set
+    :mic
+    {:script (common/plugin-script "mic.jar")
+     :update_freq 3
+     :background.color (colors/get :cream)
+     :background.corner_radius 4
+     :background.height 15
+     :icon.color (colors/get :black)
+     :icon.font (fonts/get :Medium 12.0)
+     :label.color (colors/get :black)
+     :label.font (fonts/get :Medium 12.0)})
+   (sketchybar/subscribe :mic :mouse.clicked)))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
