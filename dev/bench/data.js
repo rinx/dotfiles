f9755130bec2143f15c4ff7b8e344f0b662e9416
github-action-benchmark <github@users.noreply.github.com>
2023-09-28 17:10:52 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 355792c3b29ef406b7c99e8d649f21cfae6a1826

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695789499204,
+  "lastUpdate": 1695921052945,
   "repoUrl": "https://github.com/rinx/dotfiles",
   "entries": {
     "Benchmark": [
@@ -5162,6 +5162,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim startup time",
             "value": 142.9326,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "116073707+rinx-app[bot]@users.noreply.github.com",
+            "name": "rinx-app[bot]",
+            "username": "rinx-app[bot]"
+          },
+          "committer": {
+            "email": "116073707+rinx-app[bot]@users.noreply.github.com",
+            "name": "rinx-app[bot]",
+            "username": "rinx-app[bot]"
+          },
+          "distinct": true,
+          "id": "355792c3b29ef406b7c99e8d649f21cfae6a1826",
+          "message": "[patch] :robot: Automatically upgrade dependencies.\n\nSigned-off-by: rinx-app[bot] <116073707+rinx-app[bot]@users.noreply.github.com>",
+          "timestamp": "2023-09-28T17:00:49Z",
+          "tree_id": "66f8e3aab92375713696b2c5fab3134d715b1eb2",
+          "url": "https://github.com/rinx/dotfiles/commit/355792c3b29ef406b7c99e8d649f21cfae6a1826"
+        },
+        "date": 1695921051935,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 65.271596,
+            "unit": "ms"
+          },
+          {
+            "name": "neovim load time",
+            "value": 170.62188830000005,
+            "unit": "ms"
+          },
+          {
+            "name": "neovim startup time",
+            "value": 147.8394,
             "unit": "ms"
           }
         ]
