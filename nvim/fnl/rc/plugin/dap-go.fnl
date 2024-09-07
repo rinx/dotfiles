447fd08b686f12d18375e4021651572bd692e106
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-09-07 12:26:05 +0900

    config(nvim): add keybindings for dap-go
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nvim/fnl/rc/plugin/dap-go.fnl
+++ nvim/fnl/rc/plugin/dap-go.fnl
@@ -2,4 +2,15 @@
 
 (local dap-go (require :dap-go))
 
+(import-macros {: map!} :rc.macros)
+
 (dap-go.setup {})
+
+(vim.api.nvim_create_user_command
+  :DapGoTest "lua require('dap-go').debug_test()" {})
+
+(vim.api.nvim_create_user_command
+  :DapGoLastTest "lua require('dap-go').debug_last_test()" {})
+
+(map! [:n] "<F6>" ":<C-u>DapGoTest<CR>" {:silent true})
+(map! [:n] "<F7>" ":<C-u>DapGoLastTest<CR>" {:silent true})
--- nvim/lua/rc/plugin/dap-go.lua
+++ nvim/lua/rc/plugin/dap-go.lua
@@ -2,4 +2,8 @@
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local dap_go = require("dap-go")
-return dap_go.setup({})
+dap_go.setup({})
+vim.api.nvim_create_user_command("DapGoTest", "lua require('dap-go').debug_test()", {})
+vim.api.nvim_create_user_command("DapGoLastTest", "lua require('dap-go').debug_last_test()", {})
+vim.keymap.set("n", "<F6>", ":<C-u>DapGoTest<CR>", {silent = true})
+return vim.keymap.set("n", "<F7>", ":<C-u>DapGoLastTest<CR>", {silent = true})
