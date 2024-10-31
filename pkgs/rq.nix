2b67644b127751ccb5cb32fdd39d2efbfbb5a5ef
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-10-31 09:37:10 +0900

    chore(nix): upgrade rq to v0.0.10
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- pkgs/rq.nix
+++ pkgs/rq.nix
@@ -5,14 +5,14 @@
 }:
 buildGoModule rec {
   name = "rq";
-  version = "0.0.9";
+  version = "0.0.10";
 
   src = fetchFromSourcehut {
     owner = "~charles";
     repo = "rq";
     rev = "v${version}";
-    hash = "sha256-ak0whlawHj1BkiZ31/agdadHB1ykQSqa6avESZA0RGA=";
+    hash = "sha256-E++Ni3pN5g1c4vmP+/rOQsprlzIKhGJITbFGVitYKmU=";
   };
 
-  vendorHash = "sha256-lNUsaM7e1IsJYQFwlkhumo8i9ExOp96Li1Nszv4ce18=";
+  vendorHash = "sha256-cILZxrT0lVIK3N662zYUmDg9EfhRBynO6jYoAX6aZwo=";
 }
