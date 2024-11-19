46f772741baa40f2cb3088681f0c16a06785743d
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-11-19 18:43:14 +0900

    config(zsh): enable nix_shell prompt
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- p10k.zsh
+++ p10k.zsh
@@ -90,7 +90,7 @@
     # xplr                  # xplr shell (https://github.com/sayanarijit/xplr)
     # vim_shell             # vim shell indicator (:sh)
     # midnight_commander    # midnight commander shell (https://midnight-commander.org/)
-    # nix_shell             # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
+    nix_shell             # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
