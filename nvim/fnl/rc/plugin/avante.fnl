406e4c4d3edcb2049b0be83c900cf78473b19a49
Rintaro Okamura <rintaro.okamura@gmail.com>
2025-04-04 22:49:41 +0900

    config(nvim): use cluade-3.7-sonnet on copilot for avante
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nvim/fnl/rc/plugin/avante.fnl
+++ nvim/fnl/rc/plugin/avante.fnl
@@ -6,6 +6,8 @@
    {:auto_set_keymaps false
     :auto_suggestions false
     :auto_apply_diff_after_generation true}
+   :copilot
+   {:model :claude-3.7-sonnet}
    :hints
    {:enabled false}
    :file_selector
--- nvim/lua/rc/plugin/avante.lua
+++ nvim/lua/rc/plugin/avante.lua
@@ -1,3 +1,3 @@
 -- [nfnl] Compiled from fnl/rc/plugin/avante.fnl by https://github.com/Olical/nfnl, do not edit.
 local avante = require("avante")
-return avante.setup({provider = "copilot", behavior = {auto_apply_diff_after_generation = true, auto_set_keymaps = false, auto_suggestions = false}, hints = {enabled = false}, file_selector = {provider = "snacks"}})
+return avante.setup({provider = "copilot", behavior = {auto_apply_diff_after_generation = true, auto_set_keymaps = false, auto_suggestions = false}, copilot = {model = "claude-3.7-sonnet"}, hints = {enabled = false}, file_selector = {provider = "snacks"}})
