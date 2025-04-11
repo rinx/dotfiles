10b1b43021ae08843bc6781345fecc7fb8f4d4a1
Rintaro Okamura <rintaro.okamura@gmail.com>
2025-04-11 23:03:25 +0900

    config(nix): use nix-systems/default
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
     mcp-hub.url = "github:ravitemer/mcp-hub";
     mcp-servers-nix.url = "github:natsukium/mcp-servers-nix";
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
@@ -12,17 +13,13 @@
     inputs@{
       self,
       nixpkgs,
+      systems,
       mcp-hub,
       mcp-servers-nix,
       neovim-nightly,
     }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
     in
     {
       packages = forAllSystems (
