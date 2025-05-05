e2d3bb4a38651274d69e215a5c9fdb3a5fbb06b5
Rintaro Okamura <rintaro.okamura@gmail.com>
2025-05-05 17:02:43 +0900

    chore(nix): add git-hooks-nix to run pre-commit hook
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     mcp-hub.url = "github:ravitemer/mcp-hub";
     mcp-servers-nix.url = "github:natsukium/mcp-servers-nix";
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
 
     # non-flake packages
     ysugimoto-falco = {
@@ -28,9 +29,13 @@
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
       systems = import systems;
       perSystem =
         {
+          config,
           pkgs,
           system,
           ...
@@ -51,6 +56,20 @@
             charles-rq = inputs.charles-rq;
           };
           formatter = pkgs.nixfmt-rfc-style;
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                nixfmt-rfc-style.enable = true;
+              };
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.pre-commit.devShell
+            ];
+            packages = [ ];
+          };
         };
     };
 }
