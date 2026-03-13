31b3e9e729fc4872dc7844e1eaf2f78138afbe14
Rintaro Okamura <rintaro.okamura@gmail.com>
2026-03-13 21:54:22 +0900

    fix(zsh): fix to load plugin correctly
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- zshrc.d/zeno.zsh
+++ zshrc.d/zeno.zsh
@@ -3,6 +3,10 @@ export ZENO_HOME=$HOME/.config/zsh/rc/zeno
 export ZENO_GIT_CAT="bat --color=always"
 export ZENO_GIT_TREE="eza --tree"
 
+# NOTE: this is a dirty workaround. should be follow the official guide.
+# https://github.com/yuki-yano/zeno.zsh?tab=readme-ov-file#lazy-load-apis-for-zsh
+zeno-ensure-loaded
+
 if [[ -n $ZENO_LOADED ]]; then
   bindkey " " zeno-auto-snippet
 
