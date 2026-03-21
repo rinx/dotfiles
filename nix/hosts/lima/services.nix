c183bc3d08280329210d1d243b5b8705deb9de5b
Rintaro Okamura <rintaro.okamura@gmail.com>
2026-03-21 11:46:40 +0900

    refactor(lima-nixos): remove duplicated codes
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nix/hosts/lima/services.nix
+++ nix/hosts/lima/services.nix
@@ -5,16 +5,6 @@
   ...
 }:
 {
-  programs.home-manager.enable = true;
-
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-
-    nix-direnv.enable = true;
-  };
-
   services.gpg-agent = {
     enable = false;
   };
