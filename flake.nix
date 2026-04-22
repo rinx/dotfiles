f73b3b82e236874c4f2f2dec1a8c32d71f6c086f
Rintaro Okamura <rintaro.okamura@gmail.com>
2026-04-22 22:46:02 +0900

    chore(nix): pull gleam2nix from another repository
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
     };
 
     gleam2nix = {
-      url = "git+https://tangled.org/foxgirl.engineering/gleam2nix";
+      url = "git+https://git.isincredibly.gay/srxl/gleam2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
