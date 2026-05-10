ea9721aab98fc48341c3692269a787b201f7dc27
Rintaro Okamura <rintaro.okamura@gmail.com>
2026-05-10 12:34:06 +0900

    feat(nix/xsr-client): add `paste` client
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nix/pkgs/tools/xsr-client/default.nix
+++ nix/pkgs/tools/xsr-client/default.nix
@@ -2,6 +2,19 @@
   lib,
   pkgs,
 }:
-pkgs.writeShellScriptBin "xdg-open" ''
-  echo -n "{\"name\": \"open\", \"argument\": \"$1\"}" | nc -U ~/.xsr.sock
-''
+let
+  pbpaste = pkgs.writeShellScriptBin "pbpaste" ''
+    echo -n "{\"name\": \"paste\"}" | nc -U ~/.xsr.sock
+  '';
+
+  xdg-open = pkgs.writeShellScriptBin "xdg-open" ''
+    echo -n "{\"name\": \"open\", \"argument\": \"$1\"}" | nc -U ~/.xsr.sock
+  '';
+in
+pkgs.buildEnv {
+  name = "xsr-client";
+  paths = [
+    pbpaste
+    xdg-open
+  ];
+}
