2a679f0414e0e34215a150f3ffb75d30538d2836
Rintaro Okamura <rintaro.okamura@gmail.com>
2026-05-10 13:08:26 +0900

    chore: update xsr
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- nix/pkgs/tools/xsr-client/default.nix
+++ nix/pkgs/tools/xsr-client/default.nix
@@ -7,14 +7,19 @@ let
     echo -n "{\"name\": \"paste\"}" | nc -U ~/.xsr.sock
   '';
 
+  pngpaste = pkgs.writeShellScriptBin "pngpaste" ''
+    echo -n "{\"name\": \"pngpaste\", \"arguments\": [\"-b\"]}" | nc -U ~/.xsr.sock
+  '';
+
   xdg-open = pkgs.writeShellScriptBin "xdg-open" ''
-    echo -n "{\"name\": \"open\", \"argument\": \"$1\"}" | nc -U ~/.xsr.sock
+    echo -n "{\"name\": \"open\", \"arguments\": [\"$1\"]}" | nc -U ~/.xsr.sock
   '';
 in
 pkgs.buildEnv {
   name = "xsr-client";
   paths = [
     pbpaste
+    pngpaste
     xdg-open
   ];
 }
